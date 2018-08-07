3eb2348be6320b5477936ebed0cfcfdafef202c6
Ian Lizarda <ianlizzo@Ians-MacBook-Pro.local>
2018-08-06 21:20:23 -0700

    finished

--- appEngine-DataStore/labs/fortune-teller/start/main.py
+++ appEngine-DataStore/labs/fortune-teller/start/main.py
@@ -1,35 +1,4 @@
-#!/usr/bin/python
-#
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import jinja2
 import os
@@ -37,42 +6,31 @@ import random
 
 
 def get_fortune():
-    #add a list of fortunes to the empty fortune_list array
     fortune_list=['You will win a million dollars',
                   'Something cool will happen to you',
                   'You will have a successful career',
                   'You will die'
                  ]
-    #use the random library to return a random element from the array
     random_fortune = random.choice(fortune_list)
     return(random_fortune)
 
 
-#remember, you can get this by searching for jinja2 google app engine
-jinja_current_directory = "insert jinja2 environment variable here"
+jinja_current_directory = jinja_current_directory = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class FortuneHandler(webapp2.RequestHandler):
     def get(self):
-        # In part 2, instead of returning this string,
-        # make a function call that returns a random fortune.
-        # fortune_call = get_fortune()
-        self.response.write(get_fortune())
-    #add a post method
-    #def post(self):
+        start_template = jinja_current_directory.get_template("templates/fortune_start.html")
+        self.response.write(start_template.render())
+    def post(self):
+        random_fortune = get_fortune()
+        user_astro_sign = self.request.get("user_astrological_sign")
+        my_dict = {"the_fortune": random_fortune, "the_astro_sign": user_astro_sign}
+        results_template = jinja_current_directory.get_template("templates/fortune_results.html")
+        self.response.write(results_template.render(my_dict))
 
-class HelloHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello World. Welcome to the root route of my app')
-
-class GoodbyeHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('My response is Goodbye World')
-
-#the route mapping
 app = webapp2.WSGIApplication([
-    #this line routes the main url ('/')  - also know as
-    #the root route - to the Fortune Handler
-    ('/', HelloHandler),
-    ('/predict', FortuneHandler), #maps '/predict' to the FortuneHandler
-    ('/farewell', GoodbyeHandler)
+    ('/predict', FortuneHandler),
 ], debug=True)
